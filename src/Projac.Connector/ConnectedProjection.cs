94eb2fa3c8712c0f8e53349f0ab5658346d8f3c5
Yves Reynhout <yves.reynhout@gmail.com>
2015-05-21 16:30:17 +0200

    Aligned with latest SqlProjection class.

--- src/Projac.Connector/ConnectedProjection.cs
+++ src/Projac.Connector/ConnectedProjection.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace Projac.Connector
     /// <summary>
     ///     Represent a connected projection.
     /// </summary>
-    public abstract class ConnectedProjection<TConnection>
+    public abstract class ConnectedProjection<TConnection> : IEnumerable<ConnectedProjectionHandler<TConnection>>
     {
         private readonly List<ConnectedProjectionHandler<TConnection>> _handlers;
 
@@ -18,11 +19,6 @@ namespace Projac.Connector
         protected ConnectedProjection()
         {
             _handlers = new List<ConnectedProjectionHandler<TConnection>>();
-        }
-
-        private ConnectedProjection(ConnectedProjectionHandler<TConnection>[] handlers)
-        {
-            _handlers = new List<ConnectedProjectionHandler<TConnection>>(handlers);
         }
  
         /// <summary>
@@ -67,50 +63,125 @@ namespace Projac.Connector
         }
 
         /// <summary>
-        ///     Concatenates the handlers of this projection with the handlers of the specified projection.
+        /// Performs an implicit conversion from <see cref="ConnectedProjection{TConnection}"/> to <see><cref>ConnectedProjectionHandler{TConnection}</cref></see>.
         /// </summary>
-        /// <param name="projection">The projection to concatenate.</param>
-        /// <returns>A <see cref="ConnectedProjection{TConnection}"/> containing the concatenated handlers.</returns>
-        /// <exception cref="System.ArgumentNullException">Thrown when <paramref name="projection"/> is <c>null</c>.</exception>
-        public ConnectedProjection<TConnection> Concat(ConnectedProjection<TConnection> projection)
+        /// <param name="instance">The instance.</param>
+        /// <returns>
+        /// The result of the conversion.
+        /// </returns>
+        public static implicit operator ConnectedProjectionHandler<TConnection>[](ConnectedProjection<TConnection> instance)
         {
-            if (projection == null)
-                throw new ArgumentNullException("projection");
-            return Concat(projection.Handlers);
+            return instance.Handlers;
         }
 
         /// <summary>
-        ///     Concatenates the handlers of this projection with the specified projection handler.
+        /// Returns an enumerator that iterates through a copy of the handlers.
         /// </summary>
-        /// <param name="handler">The projection handler to concatenate.</param>
-        /// <returns>A <see cref="ConnectedProjection{TConnection}"/> containing the concatenated handlers.</returns>
-        /// <exception cref="System.ArgumentNullException">Thrown when <paramref name="handler"/> is <c>null</c>.</exception>
-        public ConnectedProjection<TConnection> Concat(ConnectedProjectionHandler<TConnection> handler)
+        /// <returns>
+        /// An <see cref="Enumerator" /> that can be used to iterate through a copy of the handlers.
+        /// </returns>
+        public Enumerator GetEnumerator()
         {
-            if (handler == null)
-                throw new ArgumentNullException("handler");
-
-            var concatenated = new ConnectedProjectionHandler<TConnection>[Handlers.Length + 1];
-            Handlers.CopyTo(concatenated, 0);
-            concatenated[Handlers.Length] = handler;
-            return new ConnectedProjection<TConnection>(concatenated);
+            return new Enumerator(Handlers);
         }
 
         /// <summary>
-        ///     Concatenates the handlers of this projection with the specified projection handlers.
+        /// Returns an enumerator that iterates through the collection.
         /// </summary>
-        /// <param name="handlers">The projection handlers to concatenate.</param>
-        /// <returns>A <see cref="ConnectedProjection{TConnection}"/> containing the concatenated handlers.</returns>
-        /// <exception cref="System.ArgumentNullException">Thrown when <paramref name="handlers"/> are <c>null</c>.</exception>
-        public ConnectedProjection<TConnection> Concat(ConnectedProjectionHandler<TConnection>[] handlers)
+        /// <returns>
+        /// An <see cref="Enumerator" /> that can be used to iterate through the collection.
+        /// </returns>
+        IEnumerator<ConnectedProjectionHandler<TConnection>> IEnumerable<ConnectedProjectionHandler<TConnection>>.GetEnumerator()
         {
-            if (handlers == null)
-                throw new ArgumentNullException("handlers");
+            return GetEnumerator();
+        }
 
-            var concatenated = new ConnectedProjectionHandler<TConnection>[Handlers.Length + handlers.Length];
-            Handlers.CopyTo(concatenated, 0);
-            handlers.CopyTo(concatenated, Handlers.Length);
-            return new ConnectedProjection<TConnection>(concatenated);
+        /// <summary>
+        /// Returns an enumerator that iterates through a collection.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="T:System.Collections.IEnumerator" /> object that can be used to iterate through the collection.
+        /// </returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>
+        /// Represents a <see cref="ConnectedProjectionHandler{TConnection}"/> array enumerator.
+        /// </summary>
+        public class Enumerator : IEnumerator<ConnectedProjectionHandler<TConnection>>
+        {
+            private readonly ConnectedProjectionHandler<TConnection>[] _handlers;
+            private int _index;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Enumerator"/> class.
+            /// </summary>
+            /// <param name="handlers">The handlers to enumerate.</param>
+            /// <exception cref="ArgumentNullException">Thrown when <paramref name="handlers"/> are <c>null</c>.</exception>
+            public Enumerator(ConnectedProjectionHandler<TConnection>[] handlers)
+            {
+                if (handlers == null) throw new ArgumentNullException("handlers");
+                _handlers = handlers;
+                _index = -1;
+            }
+
+            /// <summary>
+            /// Advances the enumerator to the next element of the collection.
+            /// </summary>
+            /// <returns>
+            /// true if the enumerator was successfully advanced to the next element; false if the enumerator has passed the end of the collection.
+            /// </returns>
+            public bool MoveNext()
+            {
+                return _index < _handlers.Length &&
+                    ++_index < _handlers.Length;
+            }
+
+            /// <summary>
+            /// Sets the enumerator to its initial position, which is before the first element in the collection.
+            /// </summary>
+            public void Reset()
+            {
+                _index = -1;
+            }
+
+            /// <summary>
+            /// Gets the element in the collection at the current position of the enumerator.
+            /// </summary>
+            /// <exception cref="System.InvalidOperationException">
+            /// Enumeration has not started. Call MoveNext.
+            /// or
+            /// Enumeration has already ended. Call Reset.
+            /// </exception>
+            public ConnectedProjectionHandler<TConnection> Current
+            {
+                get
+                {
+                    if (_index == -1)
+                        throw new InvalidOperationException("Enumeration has not started. Call MoveNext.");
+                    if (_index == _handlers.Length)
+                        throw new InvalidOperationException("Enumeration has already ended. Call Reset.");
+
+                    return _handlers[_index];
+                }
+            }
+
+            /// <summary>
+            /// Gets the element in the collection at the current position of the enumerator.
+            /// </summary>
+            object IEnumerator.Current
+            {
+                get { return Current; }
+            }
+
+            /// <summary>
+            /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+            /// </summary>
+            public void Dispose()
+            {
+            }
         }
     }
 }
