d7df5bf714233adafafa9b9b12231b9dd04787f8
Yves Reynhout <yves.reynhout@gmail.com>
2015-12-02 17:23:37 +0100

    Upgraded MSBuildTasks and NUnit.Runner (now NUnit.Console) in a desperate attempt of getting these tests to run with NUnit 3.0. Tooling still needs some love me thinks.

--- src/SharedVersionInfo.cs
+++ src/SharedVersionInfo.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyVersion("0.0.130.0")]
+[assembly: System.Reflection.AssemblyVersion("0.0.138.0")]
 
 
