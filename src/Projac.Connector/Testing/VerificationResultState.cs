d4fdead73db514073904215bedf7e8b57d0eae96
Yves Reynhout <yves.reynhout@gmail.com>
2015-12-17 00:27:02 +0100

    Initial test api design completed and documented. Now onto testing the testing api.

---
+++ src/Projac.Connector/Testing/VerificationResultState.cs
@@ -0,0 +1,17 @@
+namespace Projac.Connector.Testing
+{
+    /// <summary>
+    /// The resulting state of the test specification verification.
+    /// </summary>
+    public enum VerificationResultState
+    {
+        /// <summary>
+        /// The test specification verification passed.
+        /// </summary>
+        Passed = 0,
+        /// <summary>
+        /// The test specification verification failed.
+        /// </summary>
+        Failed
+    }
+}
