7a787943fb7d0623e71f21208f1d3046fa566997
Yves Reynhout <yves.reynhout@gmail.com>
2016-11-06 17:52:49 +0100

    SQLite ported ohne tests.

---
+++ src/Projac.SQLite/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Projac.SQLite")]
+[assembly: AssemblyDescription("This library provides lightweight infrastructure for producing SQLITE DML statements.")]
---
+++ src/Projac.SQLite/SQLiteSyntax.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.Common;
+using System.Linq;
+using System.Reflection;
+using Projac.Sql;
+
+namespace Projac.SQLite
+{
+    /// <summary>
+    ///     Fluent SQLite syntax.
+    /// </summary>
+    public partial class SQLiteSyntax
+    {
+        private static DbParameter[] CollectFromAnonymousType(object parameters)
+        {
+            if (parameters == null)
+                return new DbParameter[0];
+            return parameters.
+                    GetType().
+                    GetProperties(BindingFlags.Instance | BindingFlags.Public).
+                    Where(property => typeof(IDbParameterValue).IsAssignableFrom(property.PropertyType)).
+                    Select(property =>
+                        ((IDbParameterValue)property.GetGetMethod().Invoke(parameters, null)).
+                            ToDbParameter(FormatDbParameterName(property.Name))).
+                    ToArray();
+        }
+
+        private static string FormatDbParameterName(string name)
+        {
+            return "@" + name;
+        }
+    }
+}
