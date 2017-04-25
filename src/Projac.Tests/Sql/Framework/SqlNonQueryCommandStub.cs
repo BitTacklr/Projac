9ab2f4003bf16781b5865f584a6f1acab99a73b4
Yves Reynhout <yves.reynhout@gmail.com>
2017-04-25 20:35:13 +0200

    Fixed: Git ignore rule caused sql folder to be excluded.

---
+++ src/Projac.Tests/Sql/Framework/SqlNonQueryCommandStub.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+using System.Data.Common;
+
+namespace Projac.Sql.Tests.Framework
+{
+    public class SqlNonQueryCommandStub : SqlNonQueryCommand
+    {
+        public SqlNonQueryCommandStub(string text, DbParameter[] parameters, CommandType type) : base(text, parameters, type)
+        {
+        }
+    }
+}
