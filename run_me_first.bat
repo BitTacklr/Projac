18fc0e49e56c965526b27a8003017ee89e664d92
Yves Reynhout <yves.reynhout@gmail.com>
2017-12-04 18:31:42 +0100

    Removed obsolete files

--- run_me_first.bat
+++
@@ -1,6 +0,0 @@
-cls
-echo off
-echo ------------------------------------------------
-echo Script that prepares your working copy for usage
-echo ------------------------------------------------
-"%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe" /v:n "build\run_me_first.proj" /logger:FileLogger,Microsoft.Build.Engine;LogFile=build\run_me_first.log
